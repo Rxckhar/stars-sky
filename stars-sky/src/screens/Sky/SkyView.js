7fef435d5c695ded12f582d76d909e6c97957c85
Rxckhar <russaf007@mail.ru>
2022-10-20 15:59:56 +0500

    Добавил возможность добавлять звёзды на небо

--- stars-sky/src/screens/Sky/SkyView.js
+++ stars-sky/src/screens/Sky/SkyView.js
@@ -4,10 +4,13 @@ import { Star } from "../Star";
 import styles from "./Sky.module.scss";
 
 const SkyView = (props) => {
-    const {} = props;
+    const {stars, changeSky} = props;
     return(
-        <div className={styles._}>
+        <div className={styles._} onClick={() => changeSky()}>
             <Moon />
+            {stars.map((star, index) =>(
+                <Star key={"star_" + index}/>
+            ))}
             <Star />
         </div>
     );
--- stars-sky/src/screens/Sky/useSky.js
+++ stars-sky/src/screens/Sky/useSky.js
@@ -1,8 +1,22 @@
-import React from "react";
+import {useState} from "react";
 
 const useSky = () => {
+    const [stars, setStars] = useState([]);
+
+    const changeSky = () => {
+        const newStars = stars;
+
+        const star = {
+
+        }
+
+        newStars.push(star)
+
+        setStars([...newStars]);
+    }
     return({
-        
+        stars,
+        changeSky,
     });
 }
 
